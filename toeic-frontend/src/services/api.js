958682d4274a3b41f9d78273c486f2eed67f042b
lephilong190702 <lephilong1907@gmail.com>
2025-06-25 21:08:51 +0700

    Update

--- toeic-frontend/src/services/api.js
+++ toeic-frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 // ⚙️ Cấu hình baseURL
-const API_BASE_URL = "https://toeic-manager.up.railway.app/api";
+const API_BASE_URL = "https://toeic-manager-production.up.railway.app/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
